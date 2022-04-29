02d30d839a6fa5de59ae918bd5071c033a264e86
Roman Garkavenko <62663520+romangarkavenko9@users.noreply.github.com>
2022-04-29 11:02:37 +0000

    Add files via upload
    metoda read (posledny commit sa nepodaril)

--- reader.py
+++ reader.py
@@ -5,6 +5,36 @@ import sys
 import signal
 
 def READ(dict):
+	#kontrola hodnot from a to 
+	
+
+	try:
+		with open(f'data/{dict["File"]}', 'r') as file:
+			fileContent = file.readlines()
+			lines = len(flieContent) #pocet riadkov v subore
+			
+			if(not isinstance(dict["From"],int)) or (dict["From"] < 0 ):
+				return '','',200,'Bad request'
+
+			if(not isinstance(dict["To"],int)) or (dict["To"] < 0 ) or (dict["To"] < dict["From"]):  
+				return '','',200,'Bad request'
+			
+			if(dict["To"] > (lines - 1)):
+				return '','',201,'Bad line number'
+
+    except FileNotFoundError:
+        return '','',202,'No such file'
+    except OSError:
+        return '','',203,'Read error'
+    except KeyError:
+        return '','',200,'Bad request'
+
+    headerReply = f'Lines:{dict["To"] - dict["From"]}' # hlavicka odpovede
+    contentReply = [lines[i] for i in range(dict["From"], dict["To"])] # vybranie konkretnych riadkov zo suboru
+    contentReply = ''.join(contentReply) # spojenie riadkov do jedneho stringu 
+
+    return headerReply, contentReply, 100, 'OK'
+
 
 def LS(dict):
 
@@ -40,7 +70,7 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('', 9999))
 signal.signal(signal.SIGCHLD, signal.SIG_IGN)
 s.listen(5)
-
+qhp_Qg89vY6qQtrbMkt16Eh0H4PE5nuPyy098o6D
 while True:
     connectedSocket, address = s.accept()
     print(f'connection from adress: {address}')
